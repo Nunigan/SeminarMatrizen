bbefe2073c7db78866e77d952c75661a8a87f45b
Michael Schmid <michael.schmid2@ost.ch>
2021-03-26 17:36:43 +0100

    added code for fast MM

--- buch/papers/multiplikation/code/MM.c
+++ buch/papers/multiplikation/code/MM.c
@@ -1,29 +1,156 @@
 #include<stdio.h>
+#include"c_matrix.h"
 
-int * MM(int *A ,int *B, int n);
+void MM(const int A[][n] ,const int B[][n], int C[][n],  int n);
+void MM_dc(const int A[][n], const int B[][n], int C[][n], const int n);
 
-int main() {
-  int n = 64;
-	int A[n][n];
-  int B[n][n];
+void printMatrix(int C[][n]);
+void split(const int in[][n], int out[][n], int n, int col, int row);
+void join(const int in[][n], int out[][n], int n, int col, int row);
+void add(const int A[][n] ,const int B[][n], int C[][n], int n);
+void sub(const int A[][n] ,const int B[][n], int C[][n]);
+void multiply(const int A[][n] ,const int B[][n], int C[][n]);
+
+int main() 
+{
   int C[n][n];
-
-
+  MM_dc(A,B,C,n);
+  printMatrix(C);
 	return 0;
 }
 
-int * MM(int A[][] ,int B[][], int n)
+void MM(const int A[][n] ,const int B[][n], int C[][n],  int n)
 {
-  for(int i=0;i<n;++i)
-  {
-    for(int j=0;j<n;++j)
-    {
-      c[i][j] = 0;
-      for(int k=0;k<n,++k)
-      {
-        c[i][j] += A;
-      }
-    }
-  }
-  return C;
+	for(int i=0;i<n;++i)
+	{
+		for(int j=0;j<n;++j)
+		{
+			C[i][j] = 0;
+			for(int k=0;k<n;++k)
+			{
+				C[i][j] += A[i][k]*B[k][j];
+			}
+		}
+	}
+}
+
+
+
+void MM_dc(const int A[][n], const int B[][n], int C[][n], const int n)
+{
+		printf("entered MM_dc");
+		if(n<=2)
+		{
+	    multiply(A ,B, C);
+		}
+		else
+	  {
+    		int A11[n/2][n/2];
+				int A12[n/2][n/2];
+				int A21[n/2][n/2];
+				int A22[n/2][n/2];
+    		int B11[n/2][n/2];
+				int B12[n/2][n/2];
+				int B21[n/2][n/2];
+				int B22[n/2][n/2];
+		    
+		    split(A, A11, n/2, 0 , 0);
+        split(A, A12, n/2, 0 , n/2);
+        split(A, A21, n/2, n/2, 0);
+        split(A, A22, n/2, n/2, n/2);
+        split(B, B11, n/2, 0 , 0);
+        split(B, B12, n/2, 0 , n/2);
+ 	     	split(B, B21, n/2, n/2, 0);
+   	    split(B, B22, n/2, n/2, n/2);
+   	    		        	    		  	    		     
+				int tmp1[n/2][n/2];
+        int tmp2[n/2][n/2];
+        int tmp3[n/2][n/2];
+        int tmp4[n/2][n/2];       
+        int tmp5[n/2][n/2];
+        int tmp6[n/2][n/2];
+        int tmp7[n/2][n/2];
+        int tmp8[n/2][n/2];
+        
+               
+        MM_dc(A11, B11, tmp1, n/2);
+        MM_dc(A12, B21, tmp2, n/2);
+        MM_dc(A11, B12, tmp3, n/2);
+        MM_dc(A12, B22, tmp4, n/2);
+        MM_dc(A21, B11, tmp5, n/2);
+        MM_dc(A22, B21, tmp6, n/2);
+        MM_dc(A21, B12, tmp7, n/2);
+        MM_dc(A22, B22, tmp8, n/2);
+        
+      	int C11[n/2][n/2];
+        int C12[n/2][n/2];
+        int C21[n/2][n/2];
+        int C22[n/2][n/2];  
+        
+        add(tmp1, tmp2, C11, n/2);
+        add(tmp3, tmp4, C12, n/2);
+        add(tmp5, tmp6, C21, n/2);
+        add(tmp7, tmp8, C22, n/2);
+        
+        join(C11, C, n/2, 0, 0);
+        join(C12, C, n/2, 0, n/2);
+        join(C21, C, n/2, n/2, 0);
+        join(C22, C, n/2, n/2, n/2);
+        
+	  }
+}
+
+
+
+
+void add(const int A[][n] ,const int B[][n], int C[][n], int n)
+{
+	for (int i = 0; i < n; i++)
+	  for (int j = 0; j < n; j++)
+        C[i][j] = A[i][j] + B[i][j];
+}
+
+void sub(const int A[][n] ,const int B[][n], int C[][n])
+{
+	C[0][0] = A[0][0]-B[0][0];
+	C[0][1] = A[0][1]-B[0][1];
+	C[1][0] = A[1][0]-B[1][0];
+	C[1][1] = A[1][1]-B[1][1];
+}
+
+
+void multiply(const int A[][n] ,const int B[][n], int C[][n])
+{
+	C[0][0] = A[0][0]*B[0][0];
+	C[0][1] = A[0][1]*B[0][1];
+	C[1][0] = A[1][0]*B[1][0];
+	C[1][1] = A[1][1]*B[1][1];
+}
+
+void split(const int in[][n], int out[][n], int n, int col, int row)
+{
+	for(int i1 = 0, i2 = col; i1 < n; i1++, i2++)
+		for(int j1 = 0, j2 = row; j1 < n; j1++, j2++)
+    	out[i1][j1] = in[i2][j2];
+}
+
+void join(const int in[][n], int out[][n], int n, int col, int row)
+{
+	for(int i1 = 0, i2 = col; i1 < n; i1++, i2++)
+		for(int j1 = 0, j2 = row; j1 < n; j1++, j2++)
+    	out[i2][j2] = in[i1][j1];
+}
+
+
+
+void printMatrix(int C[][n])
+{
+	for(int i=0;i<n;++i)
+	  {
+		    for(int j=0;j<n;++j)
+		    {
+		    	printf("%d ", C[i][j]);
+		    }
+		    printf("\n");
+	   }
 }
---
+++ buch/papers/multiplikation/code/MM.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 19 07:31:29 2021
+
+@author: nunigan
+"""
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
+def MM(A, B):
+    n = np.shape(A)[0]
+    C = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            C[i, j] = 0
+            for k in range(n):
+                C[i, j] += A[i, k]*B[k, j]
+    return C
+
+
+def MM_dc(A, B):
+    n = np.shape(A)[0]
+    if(n <= 2):
+        C = np.zeros((n, n))
+        C[0, 0] = A[0, 0]*B[0, 0]+A[0, 1]*B[1, 0]
+        C[0, 1] = A[0, 0]*B[0, 1]+A[0, 1]*B[1, 1]
+        C[1, 0] = A[1, 0]*B[0, 0]+A[1, 1]*B[1, 0]
+        C[1, 1] = A[1, 0]*B[0, 1]+A[1, 1]*B[1, 1]
+        return C
+    else:
+        A11, A12, A21, A22 = A[:n//2, :n//2], A[:n//2, n//2:], A[n//2:, :n//2], A[n//2:, n//2:]
+        B11, B12, B21, B22 = B[:n//2, :n//2], B[:n//2, n//2:], B[n//2:, :n//2], B[n//2:, n//2:]
+        C11 = MM_dc(A11, B11) + MM_dc(A12, B21)
+        C12 = MM_dc(A11, B12) + MM_dc(A12, B22)
+        C21 = MM_dc(A21, B11) + MM_dc(A22, B21)
+        C22 = MM_dc(A21, B12) + MM_dc(A22, B22)
+        C = np.vstack((np.hstack((C11, C12)), np.hstack((C21, C22))))
+    return C
+
+
+def strassen(A, B):
+    n = np.shape(A)[0]
+    if(n <= 2):
+        C = np.zeros((n, n))
+        P = (A[0, 0]+A[1, 1])*(B[0, 0]+B[1, 1])
+        Q = (A[1, 0]+A[1, 1])*B[0, 0]
+        R = A[0, 0]*(B[0, 1]-B[1, 1])
+        S = A[1, 1]*(B[1, 0]-B[0, 0])
+        T = (A[0, 0]+A[0, 1])*B[1, 1]
+        U = (A[1, 0]-A[0, 0])*(B[0, 0]+B[0, 1])
+        V = (A[0, 1]-A[1, 1])*(B[1, 0]+B[1, 1])
+        C[0, 0] = P+S-T+V
+        C[0, 1] = R+T
+        C[1, 0] = Q+S
+        C[1, 1] = P+R-Q+U
+        return C
+    else:
+        m = n//2
+        A11, A12, A21, A22 = A[:m, :m], A[:m, m:], A[m:, :m], A[m:, m:]
+        B11, B12, B21, B22 = B[:m, :m], B[:m, m:], B[m:, :m], B[m:, m:]
+        C11 = strassen(A11, B11) + strassen(A12, B21)
+        C12 = strassen(A11, B12) + strassen(A12, B22)
+        C21 = strassen(A21, B11) + strassen(A22, B21)
+        C22 = strassen(A21, B12) + strassen(A22, B22)
+        print(np.shape(C11), n)
+        C = np.vstack((np.hstack((C11, C12)), np.hstack((C21, C22))))
+    return C
+
+
+def test_perfomance(n):
+    t_mm = []
+    t_mm_dc = []
+    t_mm_strassen = []
+    t_np = []
+
+    for i in n:
+        A = np.random.randn(i, i)
+        B = np.random.randn(i, i)
+
+        start = time.time()
+        C3 = strassen(A, B)
+        t_mm_strassen.append(time.time() - start)
+
+        start = time.time()
+        C1 = MM(A, B)
+        t_mm.append(time.time() - start)
+
+        start = time.time()
+        C2 = MM_dc(A, B)
+        t_mm_dc.append(time.time() - start)
+
+
+    #     start = time.time()
+    #     C = A@B
+    #     t_np.append(time.time() - start)
+
+    plt.figure()
+    plt.plot(n, t_mm, label='Standard MM', lw=5)
+    plt.plot(n, t_mm_dc, label='Divide and conquer MM', lw=5)
+    plt.plot(n, t_mm_strassen, label='Strassen MM', lw=5)
+    # plt.plot(n, t_np, label='np MM')
+    plt.legend()
+    return t_np
+
+# test%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if __name__ == '__main__':
+    n = np.logspace(1,4,4,base=2,dtype=(np.int))
+    
+    # A = np.random.randint(-10, 10, (64,64))
+    # B = np.random.randint(-10, 10, (64,64))
+
+    t_np = test_perfomance(n)
+    # C = strassen(A, B)
+    # C_test = A@B
+    
+    # def func(x, a,b,c):
+    #     return b*x**a+c
+
+    # popt, pcov = curve_fit(func, n, t_np, bounds=(0, [6,1000,1000]))
+
+
+    # plt.figure()
+    # plt.plot(n, t_np, 'b-', label='data')
+    # plt.plot(n, func(n, *popt), 'r-', label='fit: a=%5.3f , b=%5.3f, c=%5.3f' % tuple(popt))
+    # plt.xlabel('x')
+    # plt.ylabel('y')
+    # plt.legend()
